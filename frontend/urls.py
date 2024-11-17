3c16ff0ec56dd15282406b8ab5522be43cb44a61
Filip Štamcar <filip.stamcar@hotmail.com>
2024-11-17 19:32:23 +0100

    Add a proper web app manifest and description

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import AboutView, PatternsView
+from .views import AboutView, ManifestView, PatternsView
 
 urlpatterns = [
     path("", PatternsView.as_view(), name="patterns"),
     path("about/", AboutView.as_view(), name="about"),
+    path("site.webmanifest", ManifestView.as_view(), name="manifest"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,8 @@
+from django.http import JsonResponse
+from django.templatetags.static import static
+from django.views import View
 from django.views.generic import TemplateView
+from django.utils.translation import gettext_lazy as _
 
 
 class PatternsView(TemplateView):
@@ -7,3 +11,38 @@ class PatternsView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = "frontend/about.html"
+
+
+class ManifestView(View):
+    @staticmethod
+    def get(request):
+        manifest = {
+            "name": "Jelka FMF",
+            "description": _("Christmas tree at the Faculty of Mathematics and Physics, University of Ljubljana"),
+            "display": "standalone",
+            "theme_color": "#0a0c0d",
+            "background_color": "#ffffff",
+            "start_url": "/",
+            "scope": "/",
+            "icons": [
+                {
+                    "src": static("frontend/icons/favicon.svg"),
+                    "type": "image/svg+xml",
+                    "sizes": "any"
+                },
+                {
+                    "src": static("frontend/icons/favicon-maskable.svg"),
+                    "type": "image/svg+xml",
+                    "sizes": "any",
+                    "purpose": "maskable"
+                },
+                {
+                    "src": static("frontend/icons/favicon-monochrome.svg"),
+                    "type": "image/svg+xml",
+                    "sizes": "any",
+                    "purpose": "monochrome"
+                }
+            ]
+        }
+
+        return JsonResponse(manifest, content_type="application/manifest+json")
